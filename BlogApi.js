e1faaf3d9ff7c51de9b0ea0b82e4e5352f40f4ae
monica-reverte <monicareverte24@gmail.com>
2022-12-13 12:27:26 +0100

    forEach

--- BlogApi.js
+++ BlogApi.js
@@ -6,37 +6,37 @@
 const urlPost = "http://localhost:3000/posts";
 let cardBody = document.querySelector("#cardBody");
 
-fetch(urlPost)
-        .then((response) => response.json())
-        .then((posts) => {
-            const tpl = posts.map((post)=>`<div class="row row-cols-1 row-cols-md-3 g-4"><div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${post.title}</h5>
-                </div>
-            </div>
-        </div>
-        `);
-        cardBody.innerHTML = `<div class="col">${tpl}</div>`
-
-        });       
-        
-        
 //fetch(urlPost)
-       // .then(response => response.json())
-        //.then(posts => {
-            //for(let i = 0; i<data.lengtH; i++){
-              //  cardBody.innerHTML += `<div class="col"><div class="card h-100">
-               // <img src="..." class="card-img-top" alt="...">
+      //  .then((response) => response.json())
+      //  .then((posts) => {
+          //  const tpl = posts.map((post)=>`<div class="col"><div class="card h-100">
+               // <img src="" class="card-img-top" alt="...">
                 //<div class="card-body">
-                   // <h5 class="card-title">${posts.title}</h5>
+                //    <h5 class="card-title">${post.title}</h5>
                // </div>
-            //</div>
-        //`}
-            
-        //})
+           // </div>
+      //  </div>
+       // `);
+       // cardBody.innerHTML = `<div class="row row-cols-5">${tpl}</div>`
 
-//
+      //  });       
+        
+        
+fetch(urlPost)
+    .then(response => response.json())
+    .then(posts => {
+        posts.forEach(item =>{
+        cardBody.innerHTML += `<div class="col"><div class="card h-100">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${item.title}</h5>
+            </div>
+            </div>
+        `})
+            
+    })
+
+// 
 
 // const urlUsers = "http://localhost:3000/users";
 
