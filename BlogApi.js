39a7b97e19b187ae6d34029f491e45427253cec7
monica-reverte <monicareverte24@gmail.com>
2022-12-13 12:45:03 +0100

    ok

--- BlogApi.js
+++ BlogApi.js
@@ -6,21 +6,7 @@
 const urlPost = "http://localhost:3000/posts";
 let cardBody = document.querySelector("#cardBody");
 
-//fetch(urlPost)
-      //  .then((response) => response.json())
-      //  .then((posts) => {
-          //  const tpl = posts.map((post)=>`<div class="col"><div class="card h-100">
-               // <img src="" class="card-img-top" alt="...">
-                //<div class="card-body">
-                //    <h5 class="card-title">${post.title}</h5>
-               // </div>
-           // </div>
-      //  </div>
-       // `);
-       // cardBody.innerHTML = `<div class="row row-cols-5">${tpl}</div>`
 
-      //  });       
-        
         
 fetch(urlPost)
     .then(response => response.json())
@@ -36,9 +22,6 @@ fetch(urlPost)
             
     })
 
-// 
-
-// const urlUsers = "http://localhost:3000/users";
 
 
 
