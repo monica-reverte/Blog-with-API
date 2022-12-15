a6e7209904fc32d3488dc0a7516341a6da0e651f
monica-reverte <monicareverte24@gmail.com>
2022-12-15 12:16:26 +0100

    finish basic

--- BlogApi.js
+++ BlogApi.js
@@ -5,26 +5,89 @@ let cardBody = document.querySelector("#cardBody");
 fetch(urlPost)
     .then(response => response.json())
     .then(posts => {
-        posts.forEach(item =>{
-        cardBody.innerHTML += `<div class="col mb-4"><div class="card h-100">
+        posts.forEach(item => {
+            cardBody.innerHTML += `<div id="${item.id}" class="col mb-4"><div class="card h-100">
             <img src="https://picsum.photos/200/300?random=2" class="card-img-top" alt="randomImg" style="width:100%; >
-            <div class="card-body">
+            <div class="card-body active">
             <h5 class="card-title">${item.title}</h5>
             </div>
             </div>
         `})
-            
+
     })
 
+
+
+const myModal = new bootstrap.Modal("#myModal");
+
+cardBody.addEventListener("click", showModal)
+let urlModal;
+
+function showModal(event) {
+    let id;
+
+    if (event.target && event.target.parentElement.matches(".card")) {
+        myModal.show();
+        id = event.target.parentElement.parentElement.id;
+    }
+
+    urlModal = `http://localhost:3000/posts/${id}`;
     
+    fetch(urlModal)
+        .then(res => res.json())
+        .then(data => loadPost(data));
+};
 
 
+function loadPost(data) {
+    const modalTitle = document.querySelector(".modal-header");
+    const modalBody = document.querySelector(".modal-body");
+    modalTitle.innerHTML = `<h4 class="modal-title">${data.title}</h4>`;
+    modalBody.innerHTML = `<p>${data.body}</p>`;
+    loadUsers(data.userId);
+    showComments(data.id);
+};
+
+function loadUsers(data){
+    console.log(data)
+    let urlModalU = `http://localhost:3000/users/${data}`;
+
+    fetch(urlModalU)
+        .then(res=> res.json())
+        .then(data => loadEmail(data))
+}
+function loadEmail(data){
+    const modalUser = document.querySelector(".modal-user");
+    modalUser.innerHTML = `<h4>Users</h4><p>${data.username}</p><p>${data.email}</p>`;
+};
+
+const btnComments = document.querySelector(".modal-comments");
+btnComments.addEventListener("click", loadCommentsbtn);
+
+function loadCommentsbtn (){
+    let collapse = document.querySelector("#collapseEx");
+    collapse.classList.toggle("collapse"); 
+};
+
+function showComments(data){
+    const urlComments = `https://jsonplaceholder.typicode.com/posts/${data}/comments`
+    const loadCom = document.querySelector(".loading-comments")
+        fetch(urlComments)
+            .then(res => res.json())
+            .then(data => {
+                loadCom.innerHTML= "";
+                data.forEach(com => {
+                    loadCom.innerHTML += `<p>${com.name}</p><p>${com.email}</p><p>${com.body}</p>`} 
+            )}
+                    
+                    
+)}; 
+        
 
 
-
-
-
-
+        
+        
+    
 
 
 
